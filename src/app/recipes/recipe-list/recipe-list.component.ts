04be5e248a98f9da32a56c2713b927e13fc81d46
vishak7 <vishak.gowda@zysk.in>
2020-04-16 07:29:14 +0530

    Ui Changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,7 +12,7 @@ export class RecipeListComponent implements OnInit {
    recipes:Recipe[] = [
 
      new Recipe('A test recipe', 'test','https://as1.ftcdn.net/jpg/02/05/24/24/500_F_205242483_vVLU1ahNhiqDuk9bMfGrjLvhuMSOuZ0g.jpg'),
-     new Recipe('Another Status', 'test','https://as2.ftcdn.net/jpg/00/82/01/31/500_F_82013132_iBa6aHWRMJrJi8Km7LWwCxIygJojuhTS.jpg')
+     new Recipe('Another test', 'test','https://as2.ftcdn.net/jpg/00/82/01/31/500_F_82013132_iBa6aHWRMJrJi8Km7LWwCxIygJojuhTS.jpg')
 
    ];
 
