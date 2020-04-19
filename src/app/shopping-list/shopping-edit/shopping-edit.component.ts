935f77e6c466774f13f7ef0d001ee1763664177a
vishak7 <vishak.gowda@zysk.in>
2020-04-19 07:28:05 +0530

    Assignement changes

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -7,21 +7,55 @@ import { Ingredient } from 'src/app/shared/ingredient-model';
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.scss']
 })
-export class ShoppingEditComponent implements OnInit {
-  nameInput
-  amountInut
-  // @ViewChild('nameInput',{static:true}) nameInputref:ElementRef
-  // @ViewChild('amountInut',{static:true}) amountnputref:ElementRef
-  @Output() inggredietAdded = new EventEmitter <Ingredient>();
+export class ShoppingEditComponent  {
+  onAdd = true;
+  onUpdate = false;
+  ingIndex;
+  ingInfo: any = {};
 
-  constructor() { }
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 3),
+    new Ingredient('Orange', 4)
+  ]
 
-  ngOnInit() {
+  oningredientAdded() {
+
+    const newIngredient = new Ingredient(this.ingInfo.name, this.ingInfo.amount)
+
+    let indexx = this.ingredients.findIndex(x => x.name == newIngredient.name)
+
+
+    if (indexx === -1) {
+      this.ingredients.push(newIngredient)
+    }
+    else {
+      alert('Duplicate Entry');
+    }
   }
-onaddItem(){
-  const ingName = this.nameInput
-  const ingAmount = this.amountInut
-  const newIngredient = new Ingredient(ingName, ingAmount)
-  this.inggredietAdded.emit(newIngredient)
-}
+
+  onEdit(i) {
+    this.ingInfo.name = this.ingredients[i].name
+    this.ingInfo.amount = this.ingredients[i].amount
+    this.ingIndex = i
+    this.onAdd = false;
+    this.onUpdate = true;
+  }
+
+  updateIng() {
+    let editIng = this.ingIndex;
+    for (let i = 0; i < this.ingredients.length; i++) {
+      if (i == editIng) {
+        this.ingredients[i] = this.ingInfo;
+        this.ingInfo = {};
+      }
+    }
+    this.onAdd = true;
+    this.onUpdate = false
+  }
+
+  onDelete(i) {
+    this.ingredients.splice(i, 1)
+
+  }
+
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -6,51 +6,8 @@ import { Ingredient } from '../shared/ingredient-model';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
-export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[]=[
-   new Ingredient('Apples',5),
-   new Ingredient('tommatos',4)
-  ];
-  showEdit =false;
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  oningredientAdded(ingredient){
-    let indexx = this.ingredients.findIndex(x => x.name == ingredient.name)
-    if(indexx=== -1){
-      this.ingredients.push(ingredient)
-    }
-    else{
-        alert('Duplicate Entry');
-    }
-  }
-
-  onDelete(i){
-    this.ingredients.splice(i,1)
-  }
-
-  ingInfo: any = {};
+export class ShoppingListComponent {
 
 
-   myValue;
-
-  editIng(editIng) {
-    this.ingInfo.name = this.ingredients[editIng].name;
-    this.ingInfo.amount = this.ingredients[editIng].amount;
-    this.myValue = editIng;
-    this.showEdit = true;
-  }
-
-  updateIng() {
-    let editIng = this.myValue;
-    for (let i = 0; i < this.ingredients.length; i++) {
-      if (i == editIng) {
-        this.ingredients[i] = this.ingInfo;
-        this.ingInfo = {};
-      }
-    }
-  }
 
 }
